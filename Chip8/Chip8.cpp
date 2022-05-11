74c60878bfbe225d9965ee167bd0757e2373c755
arnavdandu <64711764+arnavdandu@users.noreply.github.com>
2022-05-11 01:49:15 -0400

    implemented loadROM(), Chip8(), and initializeTable()

--- Chip8/Chip8.cpp
+++ Chip8/Chip8.cpp
@@ -1,1 +1,111 @@
 #include "Chip8.h"
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <random>
+
+bit8 fontset[80] = 
+	{
+			0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+			0x20, 0x60, 0x20, 0x20, 0x70, // 1
+			0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+			0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+			0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+			0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+			0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+			0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+			0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+			0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+			0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+			0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+			0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+			0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+			0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+			0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+
+Chip8::Chip8()
+{
+	pc = 0x200;
+	randByte = std::uniform_int_distribution<bit8>(0, 225U);
+
+	for (unsigned int i = 0; i < 80; ++i)
+	{
+		memory[0x50 + i] = fontset[i];
+	}
+
+	initializeTable();
+}
+
+void Chip8::loadROM(char const* file) 
+{
+
+	// open binary stream from end
+	std::ifstream rom(file, std::ios::binary | std::ios::ate);
+
+	if (rom.is_open())
+	{
+		std::streampos size = rom.tellg(); //get size from position, which will be at the end
+		char* buffer = new char[size]; //initialize buffer using size
+
+		rom.seekg(std::ios::beg);
+		rom.read(buffer, size);
+		rom.close();
+
+		for (long i = 0; i < size; ++i)
+		{
+			memory[0x200 + i] = buffer[i]; //load to memory
+		}
+
+		delete[] buffer; buffer = nullptr;
+	} 
+	else
+	{
+		std::cerr << "Could not open file." << std::endl;
+	}
+
+}
+
+void Chip8::initializeTable()
+{
+	table[0x0] = &Chip8::Table0;
+	table[0x1] = &Chip8::OP_1nnn;
+	table[0x2] = &Chip8::OP_2nnn;
+	table[0x3] = &Chip8::OP_3xkk;
+	table[0x4] = &Chip8::OP_4xkk;
+	table[0x5] = &Chip8::OP_5xy0;
+	table[0x6] = &Chip8::OP_6xkk;
+	table[0x7] = &Chip8::OP_7xkk;
+	table[0x8] = &Chip8::Table8;
+	table[0x9] = &Chip8::OP_9xy0;
+	table[0xA] = &Chip8::OP_Annn;
+	table[0xB] = &Chip8::OP_Bnnn;
+	table[0xC] = &Chip8::OP_Cxkk;
+	table[0xD] = &Chip8::OP_Dxyn;
+	table[0xE] = &Chip8::TableE;
+	table[0xF] = &Chip8::TableF;
+	table0[0x0] = &Chip8::OP_00E0;
+	table0[0xE] = &Chip8::OP_00EE;
+	table8[0x0] = &Chip8::OP_8xy0;
+	table8[0x1] = &Chip8::OP_8xy1;
+	table8[0x2] = &Chip8::OP_8xy2;
+	table8[0x3] = &Chip8::OP_8xy3;
+	table8[0x4] = &Chip8::OP_8xy4;
+	table8[0x5] = &Chip8::OP_8xy5;
+	table8[0x6] = &Chip8::OP_8xy6;
+	table8[0x7] = &Chip8::OP_8xy7;
+	table8[0xE] = &Chip8::OP_8xyE;
+	tableE[0x1] = &Chip8::OP_ExA1;
+	tableE[0xE] = &Chip8::OP_Ex9E;
+	tableF[0x07] = &Chip8::OP_Fx07;
+	tableF[0x0A] = &Chip8::OP_Fx0A;
+	tableF[0x15] = &Chip8::OP_Fx15;
+	tableF[0x18] = &Chip8::OP_Fx18;
+	tableF[0x1E] = &Chip8::OP_Fx1E;
+	tableF[0x29] = &Chip8::OP_Fx29;
+	tableF[0x33] = &Chip8::OP_Fx33;
+	tableF[0x55] = &Chip8::OP_Fx55;
+	tableF[0x65] = &Chip8::OP_Fx65;
+}
--- Chip8/Chip8.h
+++ Chip8/Chip8.h
@@ -29,7 +29,7 @@ public:
 	Chip8();
 
 	//load ROM into memory from 
-	void loadRom(char const* fileName);
+	void loadROM(char const* file);
 
 	//fetch opcode, decode, next execute
 	void cycle();
@@ -41,6 +41,7 @@ private:
 	//These functions will dereference the pointer to the opcode functions for their table.
 	//For example, when opcode=0x00E0, table0[(0x00E0 & 0x000F)] = table0[(0x0)], which returns a pointer to Chip8::OP_00E0
 	//These tables are used because many opcodes can be grouped by their starting values: 00, 8xy, Ex, or Fx
+	void initializeTable();
 	void Table0();
 	void Table8();
 	void TableE();
@@ -48,33 +49,33 @@ private:
 
 	// Following opcode implementations are based from http://www.cs.columbia.edu/~sedwards/classes/2016/4840-spring/designs/Chip8.pdf
 
-	// Do nothing
+	// Do nothing.
 	void OP_NULL();
 
-	// CLS - clears display by setting video buffer to zeroes
+	// CLS - clears display by setting video buffer to zeroes.
 	void OP_00E0();
 
 	// RET - returns from subroutine. The interpreter sets the program counter to the address at the top of the stack,
-	// then subtracts 1 from the stack pointer
+	// then subtracts 1 from the stack pointer.
 	void OP_00EE();
 
-	// JP address - Jump to location nnn. The interpreter sets the program counter to nnn
+	// JP address - Jump to location nnn. The interpreter sets the program counter to nnn.
 	void OP_1nnn();
 
 	// CALL address - Call subroutine at nnn. The interpreter increments the stack pointer, then puts the current PC on the top
-	// of the stack. The PC is then set to nnn
+	// of the stack. The PC is then set to nnn.
 	void OP_2nnn();
 
-	// SE Vx, byte - Skip next instruction if Vx = kk. The interpreter compares register Vx to kk, and if they are equal,
-	// increments the program counter by 2
+	// SE Vx, byte - Skip next instruction if Vx = kk. The interpreter compares register Vx to kk, 
+	// and if they are equal, increments the program counter by 2.
 	void OP_3xkk();
 
-	// SNE Vx, byte - Skip next instruction if Vx != kk. The interpreter compares register Vx to kk, and if they are not equal,
-	// increments the program counter by 2
+	// SNE Vx, byte - Skip next instruction if Vx != kk. The interpreter compares register Vx to kk, 
+	// and if they are not equal, increments the program counter by 2.
 	void OP_4xkk();
 
-	// SE Vx, Vy - Skip next instruction if Vx = Vy. The interpreter compares register Vx to register Vy, and if they are equal,
-	//increments the program counter by 2
+	// SE Vx, Vy - Skip next instruction if Vx = Vy. The interpreter compares register Vx to register Vy, 
+	// and if they are equal, increments the program counter by 2.
 	void OP_5xy0();
 
 	// LD Vx, byte - Set Vx = kk. The interpreter puts the value kk into register Vx
@@ -103,65 +104,89 @@ private:
 	// subtracted from Vx, and the results stored in Vx.
 	void OP_8xy5();
 
-	// SHR Vx - Set Vx = Vx bitshifted right 1. If the least-significant (rightmost) bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2
+	// SHR Vx - Set Vx = Vx bitshifted right 1. If the least-significant (rightmost) bit of Vx is 1, then VF is set to 1, otherwise 0. 
+	// Then Vx is divided by 2
 	void OP_8xy6();
 
 	// SUBN Vx, Vy - Set Vx = Vy - Vx, set VF = NOT borrow. If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is
 	// subtracted from Vy, and the results stored in Vx.
 	void OP_8xy7();
 
-	// SHL Vx - Set Vx = Vx bitshifted left 1. If the most-significant (leftmost) bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+	// SHL Vx - Set Vx = Vx bitshifted left 1. If the most-significant (leftmost) bit of Vx is 1, then VF is set to 1, otherwise to 0. 
+	// Then Vx is multiplied by 2.
 	void OP_8xyE();
 
-	// SNE Vx, Vy - Skip next instruction if Vx != Vy. The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+	// SNE Vx, Vy - Skip next instruction if Vx != Vy. The values of Vx and Vy are compared, 
+	//  and if they are not equal, PC is increased by 2.
 	void OP_9xy0();
 
-	// LD I, address
+	// LD I, address - Set I = nnn. The value of register I is set to nnn.
 	void OP_Annn();
 
-	// JP V0, address
+	// JP V0, address - Jump to location nnn + V0. The program counter is set to nnn plus the value of V0.
 	void OP_Bnnn();
 
-	// RND Vx, byte
+	// RND Vx, byte - Set Vx = random byte AND kk. The interpreter generates a random number from 0 to 255, 
+	// which is then ANDed with the value kk. The results are stored in Vx.
 	void OP_Cxkk();
 
-	// DRW Vx, Vy, height
+	// DRW Vx, Vy, height - 
+	/*
+		Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision. The interpreter reads n
+		bytes from memory, starting at the address stored in I. These bytes are then displayed as sprites on screen
+		at coordinates (Vx, Vy). Sprites are XOR’d onto the existing screen. If this causes any pixels to be erased,
+		VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is outside the coordinates of
+		the display, it wraps around to the opposite side of the screen.
+	*/
 	void OP_Dxyn();
 
-	// SKP Vx
+	// SKP Vx - Skip next instruction if key with the value of Vx is pressed. Checks the keyboard, and  
+	// if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
 	void OP_Ex9E();
 
-	// SKNP Vx
+	// SKNP Vx - Skip next instruction if key with the value of Vx is not pressed. Checks the keyboard, and if the key
+	// corresponding to the value of Vx is currently in the up position, PC is increased by 2.
 	void OP_ExA1();
 
-	// LD Vx, DT
+	// LD Vx, DT - Set Vx = delay timer value. The value of DT is placed into Vx.
 	void OP_Fx07();
 
-	// LD Vx, K
+	// LD Vx, K - Wait for a key press, store the value of the key in Vx. All execution stops until a key is pressed, 
+	// then the value of that key is stored in Vx.
 	void OP_Fx0A();
 
-	// LD DT, Vx
+	// LD DT, Vx - Set delay timer = Vx. Delay Timer is set equal to the value of Vx.
 	void OP_Fx15();
 
-	// LD ST, Vx
+	// LD ST, Vx - Set sound timer = Vx. Sound Timer is set equal to the value of Vx.
 	void OP_Fx18();
 
-	// ADD I, Vx
+	// ADD I, Vx - Set I = I + Vx. The values of I and Vx are added, and the results are stored in I.
 	void OP_Fx1E();
 
-	// LD F, Vx
+	// LD F, Vx - 
+	/*
+		Set I = location of sprite for digit Vx. The value of I is set to the location for the hexadecimal sprite
+		corresponding to the value of Vx.See section 2.4, Display, for more information on the Chip - 8 hexadecimal
+		font. To obtain this value, multiply VX by 5 (all font data stored in first 80 bytes of memory).
+	*/
 	void OP_Fx29();
 
-	// LD B, Vx
+	// LD B, Vx - 
+	/*
+		Store BCD representation of Vx in memory locations I, I+1, and I+2. The interpreter takes the decimal
+		value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and
+		the ones digit at location I+2.
+	*/
 	void OP_Fx33();
 
-	// LD [I], Vx
+	// LD [I], Vx - Stores V0 to VX in memory starting at address I. I is then set to I + x + 1.
 	void OP_Fx55();
 
-	// LD Vx, [I]
+	// LD Vx, [I] - Fills V0 to VX with values from memory starting at address I. I is then set to I + x + 1.
 	void OP_Fx65();
 
-	std::default_random_engine randGen;
+	std::random_device randDevice;
 	std::uniform_int_distribution<bit8> randByte;
 
 	bit8 registers[16]{};
